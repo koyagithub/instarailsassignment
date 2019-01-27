f8870fea057a970c38896338961b2538ea7e289c
EC2 Default User <ec2-user@ip-172-31-40-42.us-east-2.compute.internal>
2019-01-27 07:31:45 +0000

    upload picture and fixing navbar

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,23 +1,32 @@
 class MicropostsController < ApplicationController
   before_action :require_login, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
-    @micropost = current_user.microposts.build(micropost_params)
-    if @micropost.save
-      flash[:notice] = "画像が投稿されました！"
-      redirect_to user_path(current_user)
-    else
-      @user = User.find_by(id: current_user)
-      render 'users/post'
-    end
+      @micropost = current_user.microposts.build(micropost_params)
+      if @micropost.save
+        flash[:notice] = "画像が投稿されました！"
+        redirect_to user_path(current_user)
+      else
+        @user = User.find_by(id: current_user)
+        render 'users/post'
+      end
   end
 
   def destroy
+    @micropost.destroy
+    flash[:notice] = "投稿が削除されました！"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:picture)
+    end
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
--- db/migrate/20190126115911_add_picture_to_microposts.rb
+++ db/migrate/20190126115911_add_picture_to_microposts.rb
@@ -1,5 +1,6 @@
 class AddPictureToMicroposts < ActiveRecord::Migration[5.1]
   def change
     add_column :microposts, :picture, :string
+    add_index :microposts, [:picture]
   end
 end
