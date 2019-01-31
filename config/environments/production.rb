f82f849dfaf56cbce25d0f0013aca6322c388e76
EC2 Default User <ec2-user@ip-172-31-40-42.us-east-2.compute.internal>
2019-01-31 03:22:20 +0000

    Fixing SMTP on production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,6 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ if Rails.env.production?
     if use_s3?
      config.fog_credentials = {
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :region                => ENV['ap-northeast-1'],    
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
